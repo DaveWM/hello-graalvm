645c21d0ba9e73a1e7806c7c2cf5a480c8ac698c
Michael Bragg <michael.bragg@fundingcircle.com>
2019-04-12 09:53:23 +0100

    use a graalvm Source object

--- src/hello_graalvm/core.clj
+++ src/hello_graalvm/core.clj
@@ -1,31 +1,30 @@
 (ns hello-graalvm.core
   (:require [clojure.java.io :as io]
             [clojure.edn :as edn])
-  (:import (org.graalvm.polyglot Context Value TypeLiteral))
+  (:import (org.graalvm.polyglot Context Value Source))
   (:gen-class))
 
-(def context (-> (Context/newBuilder (into-array ["R"]))
+(def permitted-languages ["R"])
+
+(def context (-> (Context/newBuilder (into-array permitted-languages))
                  (.allowAllAccess true)
                  (.build)))
 
-(defn read-file [filename]
-  (-> (io/resource filename)
-      (slurp)))
+
+(defn execute-script [filename args]
+  (let [source-file (io/file (io/resource filename))
+        source-language (Source/findLanguage source-file)
+        source-script (-> (Source/newBuilder source-language source-file)
+                          (.build))]
+    (-> (.eval context source-script)
+        (.execute (into-array args)))))
 
 
-(defn execute-script [script args]
-  (-> (.eval context "R" script)
-      (.execute (into-array args))
+(defn -main
+  [& args]
+  (println "Hello Grallvm!")
+  (-> (execute-script "example.R" [10001 10001])
       (.asString)))
 
 
-(defn run []
-  (-> (read-file "example.R")
-      (execute-script [10001 10001])))
-
-(run)
-
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+#_(-main)
